da966cd0f2e2149e0ee76c8dc1efb386c7d8e00c
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-06-09 15:07:59 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = (drivers) => {
+ return   drivers.slice(0,2);
+}
+const returnLastTwoDrivers = (drivers) => {
+    return   drivers.slice(-2);
+   }
+ const  selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+   
+ const createFareMultiplier = function (multiplier){
+return function(price){
+return multiplier*price;
+}
+} 
+function fareDoubler(fare){
+    return fare*2;
+}
+function fareTripler(fare){
+    return fare*3;
+}
+function selectDifferentDrivers(arrayOfDrivers, funct){
+    return funct(arrayOfDrivers);
+}
+
+
+
+
